adfb5c7079f0042867ddb6c4d602478277633266
Tran, Chris <ctran@mitre.org>
2020-01-13 22:50:32 -0500

    add passive C2 file ops functions and dnslib to requirements

--- app/interfaces/c2_passive_interface.py
+++ app/interfaces/c2_passive_interface.py
@@ -1,4 +1,5 @@
 import abc
+import os
 
 from abc import ABC
 
@@ -30,3 +31,31 @@ class C2Passive(ABC):
         :return:
         """
         pass
+
+    async def get_file(self, filename, platform):
+        """
+        Get file from file service and return file contents.
+
+        :param filename: File name
+        :param platform: File platform
+        """
+        if filename in self.file_svc.special_payloads:
+            f = await self.file_svc.special_payloads[filename](dict(file=filename, platform=platform))
+            return await self.file_svc.read_file(f)
+        else:
+            return await self.file_svc.read_file(filename)
+
+    async def save_file(self, filename, payload, target_dir):
+        """
+        Save a file payload into the target_dir
+
+        :param filename: Target file name
+        :param payload: File contents
+        :param target_dir: Final file path
+        """
+        try:
+            with open(os.path.join(target_dir, filename), 'wb') as f:
+                f.write(payload)
+            self.log.debug('Uploaded file %s' % filename)
+        except Exception as e:
+            self.log.debug('Exception uploading file %s' % e)
